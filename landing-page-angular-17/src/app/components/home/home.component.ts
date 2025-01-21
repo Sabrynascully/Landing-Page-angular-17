86fa84a86799c02714b2be5573507aaa87e361b5
Igor Marques <igor.marques@brconsulting.com.br>
2025-01-21 18:42:40 -0300

    construindo home html e css e configurando estilos globais

--- landing-page-angular-17/src/app/components/home/home.component.ts
+++ landing-page-angular-17/src/app/components/home/home.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    NgOptimizedImage,
+  ],
+  providers: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
