8fa69fd4b383978e292a700e8ea3aeeb2e582c45
Igor Marques <igor.marques@brconsulting.com.br>
2025-01-22 13:24:36 -0300

    criando outros components e configurações

--- landing-page-angular-17/src/app/components/home/home.component.ts
+++ landing-page-angular-17/src/app/components/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
+import { BenefitComponent } from '../benefit/benefit.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +12,10 @@ import { NgOptimizedImage } from '@angular/common';
   imports: [
     HeaderComponent,
     NgOptimizedImage,
+    BtnPrimaryComponent,
+    NewsletterFormComponent,
+    BenefitComponent,
+    FooterComponent
   ],
   providers: [],
   templateUrl: './home.component.html',
